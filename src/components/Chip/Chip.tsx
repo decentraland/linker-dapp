111c56366b78e838861dcfc196c218b26f12457f
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2023-03-03 11:58:27 -0500

    fix: Visual fixes + payload that expires and disable the sign button (#26)
    * fix: Visual fixes + payload that expires and disable the sign button
    
    * fix: Remove localhost from reqs
    
    * fix: Fix build
    
    * fix: Access control list instead of ACL
    
    * fix: Visual fixes
    
    * fix: Visual fix in the grant/revoke list of addresses

---
+++ src/components/Chip/Chip.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react'
+import { Icon } from 'decentraland-ui'
+import classNames from 'classnames'
+
+import { Props } from './Chip.types'
+
+import './styles.css'
+
+const Chip = (props: Props) => {
+  const { type, text, icon, className, isActive, isDisabled, onClick } = props
+
+  if (!text && !icon) {
+    throw new Error('You need to provide at least one prop: text or icon')
+  }
+
+  const handleKeyDown = (evt: React.KeyboardEvent<HTMLDivElement>) => {
+    if (evt.key === 'Enter' && onClick) {
+      onClick(evt)
+    }
+  }
+
+  const containerClass = classNames('Chip', type, className, {
+    active: isActive,
+    disabled: isDisabled,
+    clickeable: onClick && !isDisabled,
+  })
+
+  return (
+    <div
+      className={containerClass}
+      tabIndex={0}
+      onClick={isActive || isDisabled ? undefined : onClick}
+      onKeyDown={handleKeyDown}
+    >
+      {text ? (
+        <span className="text">{text}</span>
+      ) : icon ? (
+        <Icon name={icon} />
+      ) : null}
+    </div>
+  )
+}
+
+Chip.defaultProps = {
+  text: '',
+  icon: '',
+  type: 'square',
+  isDisabled: false,
+  isActive: false,
+}
+
+export default React.memo(Chip)
