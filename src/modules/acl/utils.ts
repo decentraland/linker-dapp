75b788964d314ce1bd5b97bc34ff55d98117e957
Kevin Szuchet <kevin.szuchet@decentraland.org>
2023-03-03 10:16:34 -0500

    fix: Remove localhost from reqs

--- src/modules/acl/utils.ts
+++ src/modules/acl/utils.ts
@@ -2,7 +2,7 @@ import { ACLResponse } from './reducer'
 import { InfoResponse, UpdatePayload } from './types'
 
 export async function getInfoRequest(): Promise<InfoResponse> {
-  return (await fetch('http://localhost:8000/api/acl')).json()
+  return (await fetch('/api/acl')).json()
 }
 
 export async function getWorldACL(
@@ -13,7 +13,7 @@ export async function getWorldACL(
 }
 
 export async function updateWorldACL(payload: UpdatePayload): Promise<void> {
-  await fetch(`http://localhost:8000/api/acl`, {
+  await fetch(`/api/acl`, {
     method: 'post',
     headers: {
       'Content-Type': 'application/json',
