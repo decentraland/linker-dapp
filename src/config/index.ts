f70bebd521b7244caf8f178ec7bdd0293851b95e
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-09-06 17:37:01 -0300

    feat: Update env config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,16 @@
-import { createConfig } from '@dcl/ui-env'
+import { Env, createConfig } from '@dcl/ui-env'
 import dev from './env/dev.json'
 import prod from './env/prod.json'
 
-export const config = createConfig({
-  dev,
-  prod
-})
+export const config = createConfig(
+  {
+    [Env.DEVELOPMENT]: dev,
+    [Env.PRODUCTION]: prod,
+  },
+  {
+    systemEnvVariables: {
+      REACT_APP_DCL_DEFAULT_ENV:
+        process.env.VITE_REACT_APP_DCL_DEFAULT_ENV ?? 'dev',
+    },
+  }
+)
